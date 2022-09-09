1d8be8a1cb2403f745cb13d4f315cfe7f88fb290
Corentin Le Molgat <corentinl@google.com>
2022-09-09 15:17:44 +0200

    windows test2

--- dump/include/dump/dump.hpp
+++ dump/include/dump/dump.hpp
@@ -34,34 +34,48 @@
 #include <vector>
 
 /* need extra level to force extra eval */
-#define DUMP_CONCATENATE(a, b) DUMP_CONCATENATE1(a, b)
-#define DUMP_CONCATENATE1(a, b) DUMP_CONCATENATE2(a, b)
-#define DUMP_CONCATENATE2(a, b) a##b
+//#define DUMP_CONCATENATE(a, b) DUMP_CONCATENATE1(a, b)
+//#define DUMP_CONCATENATE1(a, b) DUMP_CONCATENATE2(a, b)
+//#define DUMP_CONCATENATE2(a, b) a##b
+//
+//#define DUMP_EXPAND(x) x
+//#define DUMP_NARG(...) DUMP_NARG_(__VA_ARGS__ __VA_OPT__(, ) DUMP_RSEQ_N())
+//#define DUMP_NARG_(...) DUMP_EXPAND(DUMP_ARG_N(__VA_ARGS__))
+//#define DUMP_ARG_N(_1, _2, _3, _4, _5, _6, _7, _8, N, ...) N
+//#define DUMP_RSEQ_N() 8, 7, 6, 5, 4, 3, 2, 1, 0
+//
+//#define DUMP_FOR_EACH_N0(F)
+//#define DUMP_FOR_EACH_N1(F, a) F(a)
+//#define DUMP_FOR_EACH_N2(F, a, ...) F(a) DUMP_EXPAND(DUMP_FOR_EACH_N1(F, __VA_ARGS__))
+//#define DUMP_FOR_EACH_N3(F, a, ...) F(a) DUMP_EXPAND(DUMP_FOR_EACH_N2(F, __VA_ARGS__))
+//#define DUMP_FOR_EACH_N4(F, a, ...) F(a) DUMP_EXPAND(DUMP_FOR_EACH_N3(F, __VA_ARGS__))
+//#define DUMP_FOR_EACH_N5(F, a, ...) F(a) DUMP_EXPAND(DUMP_FOR_EACH_N4(F, __VA_ARGS__))
+//#define DUMP_FOR_EACH_N6(F, a, ...) F(a) DUMP_EXPAND(DUMP_FOR_EACH_N5(F, __VA_ARGS__))
+//#define DUMP_FOR_EACH_N7(F, a, ...) F(a) DUMP_EXPAND(DUMP_FOR_EACH_N6(F, __VA_ARGS__))
+//#define DUMP_FOR_EACH_N8(F, a, ...) F(a) DUMP_EXPAND(DUMP_FOR_EACH_N7(F, __VA_ARGS__))
+//#define DUMP_FOR_EACH_(N, F, ...) DUMP_EXPAND(DUMP_CONCATENATE(DUMP_FOR_EACH_N, N)(F __VA_OPT__(, ) __VA_ARGS__))
+//#define DUMP_FOR_EACH(F, ...) DUMP_FOR_EACH_(DUMP_NARG(__VA_ARGS__), F, __VA_ARGS__)
 
-#define DUMP_EXPAND(x) x
-#define DUMP_NARG(...) DUMP_NARG_(__VA_ARGS__ __VA_OPT__(, ) DUMP_RSEQ_N())
-#define DUMP_NARG_(...) DUMP_EXPAND(DUMP_ARG_N(__VA_ARGS__))
-#define DUMP_ARG_N(_1, _2, _3, _4, _5, _6, _7, _8, N, ...) N
-#define DUMP_RSEQ_N() 8, 7, 6, 5, 4, 3, 2, 1, 0
+#define DUMP_PARENS ()
 
-#define DUMP_FOR_EACH_N0(F)
-#define DUMP_FOR_EACH_N1(F, a) F(a)
-#define DUMP_FOR_EACH_N2(F, a, ...) F(a) DUMP_EXPAND(DUMP_FOR_EACH_N1(F, __VA_ARGS__))
-#define DUMP_FOR_EACH_N3(F, a, ...) F(a) DUMP_EXPAND(DUMP_FOR_EACH_N2(F, __VA_ARGS__))
-#define DUMP_FOR_EACH_N4(F, a, ...) F(a) DUMP_EXPAND(DUMP_FOR_EACH_N3(F, __VA_ARGS__))
-#define DUMP_FOR_EACH_N5(F, a, ...) F(a) DUMP_EXPAND(DUMP_FOR_EACH_N4(F, __VA_ARGS__))
-#define DUMP_FOR_EACH_N6(F, a, ...) F(a) DUMP_EXPAND(DUMP_FOR_EACH_N5(F, __VA_ARGS__))
-#define DUMP_FOR_EACH_N7(F, a, ...) F(a) DUMP_EXPAND(DUMP_FOR_EACH_N6(F, __VA_ARGS__))
-#define DUMP_FOR_EACH_N8(F, a, ...) F(a) DUMP_EXPAND(DUMP_FOR_EACH_N7(F, __VA_ARGS__))
-#define DUMP_FOR_EACH_(N, F, ...) DUMP_EXPAND(DUMP_CONCATENATE(DUMP_FOR_EACH_N, N)(F __VA_OPT__(, ) __VA_ARGS__))
-#define DUMP_FOR_EACH(F, ...) DUMP_FOR_EACH_(DUMP_NARG(__VA_ARGS__), F, __VA_ARGS__)
+#define DUMP_EXPAND(...) DUMP_EXPAND4(DUMP_EXPAND4(DUMP_EXPAND4(DUMP_EXPAND4(__VA_ARGS__))))
+#define DUMP_EXPAND4(...) DUMP_EXPAND3(DUMP_EXPAND3(DUMP_EXPAND3(DUMP_EXPAND3(__VA_ARGS__))))
+#define DUMP_EXPAND3(...) DUMP_EXPAND2(DUMP_EXPAND2(DUMP_EXPAND2(DUMP_EXPAND2(__VA_ARGS__))))
+#define DUMP_EXPAND2(...) DUMP_EXPAND1(DUMP_EXPAND1(DUMP_EXPAND1(DUMP_EXPAND1(__VA_ARGS__))))
+#define DUMP_EXPAND1(...) __VA_ARGS__
+
+#define DUMP_FOR_EACH(macro, ...)                                    \
+  __VA_OPT__(DUMP_EXPAND(DUMP_FOR_EACH_HELPER(macro, __VA_ARGS__)))
+#define DUMP_FOR_EACH_HELPER(macro, a1, ...)                         \
+  macro(a1)                                                     \
+  __VA_OPT__(DUMP_FOR_EACH_AGAIN DUMP_PARENS (macro, __VA_ARGS__))
+#define DUMP_FOR_EACH_AGAIN() DUMP_FOR_EACH_HELPER
+
 
 #define DUMP(...) DUMP_INTERNAL((), __VA_ARGS__)
 
 /* need extra level to force extra eval */
-#define DUMP_STRINGIZE(a) DUMP_STRINGIZE1(a)
-#define DUMP_STRINGIZE1(a) DUMP_STRINGIZE2(a)
-#define DUMP_STRINGIZE2(a) #a,
+#define DUMP_STRINGIZE(a) #a,
 #define DUMP_STRINGIFY(...) DUMP_FOR_EACH(DUMP_STRINGIZE, __VA_ARGS__)
 
 // Returns the arguments.
@@ -69,11 +83,8 @@
 // Removes parenthesis. Requires argument enclosed in parenthesis.
 #define DUMP_RM_PARENS(...) DUMP_IDENTITY __VA_ARGS__
 
-#define DUMP_GEN_ONE_BINDING(a) DUMP_GEN_ONE_BINDING1(a)
-#define DUMP_GEN_ONE_BINDING1(a) DUMP_GEN_ONE_BINDING2(a)
-#define DUMP_GEN_ONE_BINDING2(a) , &a = a
-#define DUMP_GEN_BINDING(binding) \
-  &DUMP_FOR_EACH(DUMP_GEN_ONE_BINDING, DUMP_RM_PARENS(binding))
+#define DUMP_GEN_ONE_BINDING(a) , &a = a
+#define DUMP_GEN_BINDING(binding) &DUMP_FOR_EACH(DUMP_GEN_ONE_BINDING, DUMP_RM_PARENS(binding))
 
 #define DUMP_INTERNAL(binding, ...)                    \
   ::dump::internal_dump::make_dump<>(                  \
